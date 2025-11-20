0bc338628c93e2ed68f83611b7399cabd724609a
Gilson96 <grafael99@gmail.com>
2025-11-20 16:06:44 +0000

    Created home component

---
+++ src/Home/home.tsx
@@ -0,0 +1,10 @@
+const Home = () => {
+  return (
+    <main className="home-main-container">
+      <button className="home-btn-go-to-game">Go to Game</button>
+      <button className="home-btn-go-to-game">View Leaderboard</button>
+    </main>
+  );
+};
+
+export default Home;
