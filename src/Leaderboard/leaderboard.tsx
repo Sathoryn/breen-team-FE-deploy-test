174f7f691460539ee1617c41ee05aeb9f8c24a99
Gilson96 <grafael99@gmail.com>
2025-11-20 16:18:57 +0000

    add navigator to the leaderboard component

--- src/Leaderboard/leaderboard.tsx
+++ src/Leaderboard/leaderboard.tsx
@@ -1,7 +1,14 @@
-import React from "react";
+import Navigator from "../Home/Navigator/navigator";
+import ScoreCard from "./scoreCard";
 
 const Leaderboard = () => {
-  return <div>Leaderboard</div>;
+  return (
+    <main className="leaderboard-main-container">
+      <h1>Leaderboard</h1>
+      <ScoreCard />
+      <Navigator />
+    </main>
+  );
 };
 
 export default Leaderboard;
