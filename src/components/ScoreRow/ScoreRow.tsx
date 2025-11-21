ec69ecc0318029c53259ed4d1ada1a28988ee834
Gilson96 <grafael99@gmail.com>
2025-11-21 10:34:59 +0000

    Declare an array of score

--- src/components/ScoreRow/ScoreRow.tsx
+++ src/components/ScoreRow/ScoreRow.tsx
@@ -1,7 +1,18 @@
 import './ScoreRow.css';
+import type { ScoreRowTypes } from '../../utils/dataTypes';
 
 const ScoreRow = () => {
-  return <div className='scoreRow'>ScoreRow</div>;
+  const scoreRow: ScoreRowTypes[] = [];
+  return (
+    <div className='scoreRow'>
+      {scoreRow.map(score => (
+        <li>
+          <p>{score.username}</p>
+          <p>{score.score}</p>
+        </li>
+      ))}
+    </div>
+  );
 };
 
 export default ScoreRow;
