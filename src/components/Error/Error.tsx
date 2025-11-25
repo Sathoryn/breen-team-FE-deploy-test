c5d77db889c0a47f5cfd03cc93fa9915c45df235
Dan Moffat <danmoffat@gmail.com>
2025-11-25 11:14:44 +0000

    Add loading and error components

---
+++ src/components/Error/Error.tsx
@@ -0,0 +1,7 @@
+import './Error.css';
+
+const Error = () => {
+  return <h1>Error works!</h1>;
+};
+
+export default Error;
---
+++ src/components/Loading/Loading.tsx
@@ -0,0 +1,7 @@
+import './Loading.css';
+
+const Loading = () => {
+  return <h1>Loading works!</h1>;
+};
+
+export default Loading;
