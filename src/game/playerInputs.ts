c243ae58c37e0b2b44fdb5744575b75eeb2b1bef
Sathoryn <103369017+Sathoryn@users.noreply.github.com>
2025-11-28 17:29:35 +0000

    Refactor player input handling for jump actions

--- src/game/playerInputs.ts
+++ src/game/playerInputs.ts
@@ -1,23 +1,21 @@
 import type { AudioPlay, KAPLAYCtx, GameObj } from 'kaplay';
 
 export function playerInputs(k: KAPLAYCtx, player: GameObj, running: AudioPlay) {
-k.ready(() =>{
+  window.addEventListener('keydown', (e) => {
+    if (e.code === 'Space' && player.isGrounded()) {
+      running.paused = true;
+      player.jump(1500);
+    }
+  });
       
-      k.onKeyPown('Space', () => {
-      if (player.isGrounded()) {
-        running.paused = true;
-        player.jump(1500);
-      }
-    });
-    k.onClick(() => {
-      if (player.isGrounded()) {
-        running.paused = true;
-        player.jump(1500);
-      }
-    });
-})
-  player.onUpdate(() => {
+  k.onClick(() => {
+    if (player.isGrounded()) {
+      running.paused = true;
+      player.jump(1500);
+    }
+  });
 
+  player.onUpdate(() => {
     if (running.paused === true) {
       k.wait(0.6, () => {
         running.paused = false;
