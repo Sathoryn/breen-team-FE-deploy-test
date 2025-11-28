64a5f683e5129ef42536cf10277902f4c366ec41
Dan Moffat <danmoffat@gmail.com>
2025-11-28 12:10:55 +0000

    Update api url

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,6 +1,6 @@
 import type { Score } from '../types';
 
-const BASE_URL = 'https://breen-team-backend.vercel.app/api/';
+const BASE_URL = 'https://breen-team-backend-navy.vercel.app/api/api/';
 
 export const getScores = async (): Promise<{ scores: Score[] }> => {
   const response = await fetch(`${BASE_URL}games/1/scores`);
